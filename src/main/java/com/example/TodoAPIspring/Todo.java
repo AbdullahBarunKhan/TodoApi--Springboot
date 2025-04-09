eb8c4bf985a451f163ecc09bf48637bd51322282
AbdullahKhan <barunabk31@gmail.com>
2025-04-09 16:31:02 +0530

    Added todo mapping

--- src/main/java/com/example/TodoAPIspring/Todo.java
+++ src/main/java/com/example/TodoAPIspring/Todo.java
@@ -6,6 +6,8 @@ public class Todo {
 	private String title;
 	private int userId;
 
+	 public Todo() {}
+	
 	public void setId(int id){
 		this.id = id;
 	}
--- src/main/java/com/example/TodoAPIspring/TodoController.java
+++ src/main/java/com/example/TodoAPIspring/TodoController.java
@@ -3,18 +3,36 @@ package com.example.TodoAPIspring;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TodoController {
 
-	public static List<Todo> todo;
+	public static List<Todo> todoList;
 	
 	public TodoController() {
 		
-		todo = new ArrayList<>();
+		todoList = new ArrayList<>();
 		
-		todo.add(new Todo(1, false, "Task 1", 001));
-		todo.add(new Todo(2, false, "Task 1", 002));
+		todoList.add(new Todo(1, false, "Task 1", 001));
+		todoList.add(new Todo(2, false, "Task 1", 002));
+	}
+	
+	@GetMapping("/todos")
+	public List<Todo> getTodoList(){
+		
+		return todoList;
+	}
+	
+	@PostMapping("/todos")
+	public Todo createTodo(@RequestBody Todo newTodo) {
+		
+		todoList.add(newTodo);
+		return newTodo;
 	}
 }
+
+
